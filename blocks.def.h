122561f1ecf85809a947ae530e2e4a351d84af9d
Prajith PA <codejs100@gmail.com>
2022-08-13 15:56:07 -0700

    Addes DWM block

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb_pacupdate",	300, 1},
+	{"",	"sb_uptime",	60,	1},
 	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-  	{"",	"sb_battery",	5,	3},
+  	{"",	"sb_volume",	5,	0},
+	{"",	"sb_battery",	5,	3},
 	{"",	"sb_clock ",	60,	1},
 };
 
